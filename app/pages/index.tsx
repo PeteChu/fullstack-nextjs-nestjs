75be1772f9c2dbfb6a616e811ab5dbb60a642f69
PeteChu <phuengton.ch@gmail.com>
2022-09-20 19:09:38 +0700

    integrated with tailwind-css

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <div>Hello; World</div>;
+  return (
+    <div className="container flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default Home;
