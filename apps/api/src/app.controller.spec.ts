a91e9cd6101b380fcb1911c1c9078505152d038b
PeteChu <phuengton.ch@gmail.com>
2022-09-29 22:06:29 +0700

    lint

--- apps/api/src/app.controller.spec.ts
+++ apps/api/src/app.controller.spec.ts
@@ -3,20 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+    let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+        }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "John Doe"', () => {
-      expect(appController.getHello()).toStrictEqual({ message: 'John Doe' });
+        appController = app.get<AppController>(AppController);
     });
-  });
+
+    describe('root', () => {
+        it('should return "John Doe"', () => {
+            expect(appController.getHello()).toStrictEqual({
+                message: 'John Doe',
+            });
+        });
+    });
 });
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return { message: 'John Doe' };
-  }
+    getHello() {
+        return { message: 'John Doe' };
+    }
 }
